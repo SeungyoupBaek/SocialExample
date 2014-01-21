d8a6e353f95e51dfd81b21dd4fa2b056400c8eb2
SeungyoupBaek <bsy8220@naver.com>
2014-01-21 16:02:35 +0900

    친구 리스트 구 완료하였습니다.

---
+++ SocialExample/TimelineViewController.m
@@ -0,0 +1,127 @@
+//
+//  TimelineViewController.m
+//  SocialExample
+//
+//  Created by SDT-1 on 2014. 1. 21..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "TimelineViewController.h"
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
+
+#define FACEBOOK_APPID @"243905615771288"
+
+@interface TimelineViewController ()<UITableViewDataSource, UITabBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *table;
+@property (strong, nonatomic) NSArray *data;
+@property (weak, nonatomic) ACAccount *facebookAccount;
+
+@end
+
+@implementation TimelineViewController
+
+-(void)requestFeed{
+    NSString *urlStr = @"https://graph.facebook.com/me/feed";
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSDictionary *params =nil;
+    SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeFacebook requestMethod:SLRequestMethodGET URL:url parameters:params];
+    request.account = self.facebookAccount;
+    
+    [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error){
+        if (nil != error) {
+            NSLog(@"Error : %@", error);
+            return;
+        }
+        
+        
+        __autoreleasing NSError *parseError;
+        NSDictionary *result = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&parseError];
+        
+        self.data = result[@"data"];
+        // 메인 쓰레드에서 화면 업데이트
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            [self.table reloadData];
+        }];
+    }];
+}
+
+-(void)showTimeline{
+    ACAccountStore *store = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+    NSDictionary *options = @{ACFacebookAppIdKey : FACEBOOK_APPID,
+                              ACFacebookPermissionsKey : @[@"read_stream"],
+                              ACFacebookAudienceKey : ACFacebookAudienceEveryone};
+    
+    [store requestAccessToAccountsWithType:accountType options:options completion:^(BOOL granted, NSError *error){
+        if (error) {
+            NSLog(@"Error : %@", error);
+        }
+        if (granted) {
+            NSLog(@"권한 승인 성공");
+            NSArray *accountList = [store accountsWithAccountType:accountType];
+            self.facebookAccount = [accountList lastObject];
+            
+            // 피드 정보를 요청한다.
+            [self requestFeed];
+        }
+        else{
+            NSLog(@"권한 승인 실패");
+        }
+            }];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.data count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FEED_CELL"];
+    
+    NSDictionary *one = self.data[indexPath.row];
+    
+    // feed는 사용자가 올린 글에 해당하는 message 와 like 등과 같은 이벤트 story로 나뉜다.
+    NSString *contents;
+    if (one[@"message"]) {
+        // 메세지인 경우에는 like의 개수
+        NSDictionary *likes = one[@"likes"];
+        NSArray *data = likes[@"data"];
+        //NSLog(@"message likes : %@ - %@", likes, count);
+        contents = [NSString stringWithFormat:@"%@ ...(%d)", one[@"message"], (int)[data count]];
+    }else{
+        contents = one[@"story"];
+        cell.indentationLevel = 2;
+    }
+    
+    cell.textLabel.text = contents;
+    return cell;
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self showTimeline];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- SocialExample/ViewController.m
+++ SocialExample/ViewController.m
@@ -7,12 +7,101 @@
 //
 
 #import "ViewController.h"
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
+
+#define FACEBOOK_APPID @"243905615771288"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *genderLabel;
+@property (weak, nonatomic) IBOutlet UILabel *linkLabel;
+@property (weak, nonatomic) IBOutlet UILabel *updateLabel;
+@property (weak, nonatomic) IBOutlet UITextView *aboutView;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+@property (strong, nonatomic) ACAccount *facebookAccount;
 @end
 
 @implementation ViewController
+
+-(void)requestProfile{
+    // 페이스북 서비스 타입 지정
+    NSString *serviceType = SLServiceTypeFacebook;
+    
+    // 정보를 얻어오기 위한 GET
+    SLRequestMethod method = SLRequestMethodGET;
+    
+    // 그래프 API를 적용한 URL
+    NSURL *url = [NSURL URLWithString:@"https://graph.facebook.com/me"];
+    
+    // 파라미터
+    NSDictionary *param = @{@"fields": @"picture,name,about,gender,link,updated_time"};
+    SLRequest *request = [SLRequest requestForServiceType:serviceType requestMethod:method URL:url parameters:param];
+    request.account = self.facebookAccount;
+    
+    
+    // 페이스북 서비스에 요청을 수행한다. 핸들러를 이용해서 요청의 결과를 처리한다.
+    [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error){
+        if (nil != error) {
+            NSLog(@"프로필 정보 얻기 실패 : %@", error);
+            return;
+        }
+        // 요청 결과를 처리하기 위한 코드 작성
+        __autoreleasing NSError *parseError = nil;
+        NSDictionary *result = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&parseError];
+        
+        NSDictionary *picture = result[@"picture"][@"data"];
+        NSString *imageUrlStr = picture[@"url"];
+        
+        NSURL *url = [NSURL URLWithString:imageUrlStr];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        UIImage *image = [UIImage imageWithData:data];
+        
+        // 메인 쓰레드에서 UI 업데이트
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            self.nameLabel.text = result[@"name"];
+            self.aboutView.text = result[@"about"];
+            self.genderLabel.text = result[@"gender"];
+            self.updateLabel.text = result[@"updated_time"];
+            self.linkLabel.text = result[@"link"];
+            self.profileImage.image = image;
+        }];
+    }];
+}
+
+
+-(void)showMyProfile{
+    // 계정 정보를 담은 스토어 객체 생성
+    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+    // 페이스북의 계정 타입
+    ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+    
+    // 앱 아이디와 권한 지정
+    NSDictionary *options = @{ACFacebookAppIdKey: FACEBOOK_APPID,
+                              ACFacebookPermissionsKey : @[@"basic_info"],
+                              ACFacebookAudienceKey : ACFacebookAudienceEveryone};
+    // 계정과 권한에 대한 승인 요청
+    [accountStore requestAccessToAccountsWithType:accountType options:options completion:
+     ^(BOOL granted, NSError *error){
+         // 승인 요청 결과를 처리할 핸들러(블록)
+         if (granted) {
+             // 승인 성공
+             NSArray *accounts = [accountStore accountsWithAccountType:accountType];
+             self.facebookAccount = [accounts lastObject];
+             
+             [self requestProfile];
+         }
+         else{
+             // 승인 실패
+             NSLog(@"승인 실패 : %@", error);
+         }
+     }];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self showMyProfile];
+}
 
 - (void)viewDidLoad
 {
